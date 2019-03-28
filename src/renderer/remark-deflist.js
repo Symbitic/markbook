a5afbe940c2a46358137241682aba3fa81c6e7d1
Alex Shaw <alex.shaw.as@gmail.com>
2019-03-28 20:02:54 +0000

    fix: rewrite deflist plugin to use pandoc syntax

--- src/renderer/remark-deflist.js
+++ src/renderer/remark-deflist.js
@@ -2,39 +2,25 @@
  * Remark DefinitionList plugin.
  * @todo release this.
  */
-import map from 'unist-util-map'
+
 import visit from 'unist-util-visit'
+import toString from 'mdast-util-to-string'
+
+const isdeflist = (node, i, parent) =>
+  i > 0 &&
+  /^:\s/.test(toString(node)) &&
+  !/^:\s/.test(toString(parent.children[i - 1])) &&
+  node.type === 'paragraph' &&
+  parent.children[i - 1].type === 'paragraph'
 
 export default function deflist (options = {}) {
   return (tree, file) => {
-    map(tree, node => {
-      if (node.type !== 'paragraph') {
-        return node
-      }
-
-      const children = node.children.filter(item => item.type === 'text')
-      let isdef = false
-      children.forEach(n => {
-        if (!isdef) {
-          isdef = /^:\s/.test(n.value)
-        }
-      })
+    // console.log(tree)
+    visit(tree, ['paragraph'], (node, i, parent) => {
+      const isdef = isdeflist(node, i, parent)
       if (!isdef) {
-        return node
+        return
       }
-
-      node.type = 'dl'
-      node.data = {
-        ...node.data,
-        hName: 'dl'
-      }
-      node.children = children.map((child, i) => ({
-        type: i % 2 === 0 ? 'dt' : 'dd',
-        children: [child],
-        data: {
-          hName: i % 2 === 0 ? 'dt' : 'dd'
-        }
-      }))
 
       // Remove the ": " that we use to identify a deflist to begin with.
       visit(node, n => {
@@ -43,7 +29,35 @@ export default function deflist (options = {}) {
         }
       })
 
-      return node
+      const child = {
+        type: 'paragraph',
+        children: [
+          {
+            type: 'descriptionlist',
+            data: {
+              hName: 'dl'
+            },
+            children: [
+              {
+                type: 'descriptionterm',
+                data: {
+                  hName: 'dt'
+                },
+                children: parent.children[i - 1].children
+              },
+              {
+                type: 'descriptiondetails',
+                data: {
+                  hName: 'dd'
+                },
+                children: node.children
+              }
+            ]
+          }
+        ]
+      }
+
+      parent.children.splice(i - 1, 2, child)
     })
   }
 }
